77199251c6e71e43233885429288578c4337a230
RostislavFrankiv <32019542+RostislavFrankiv@users.noreply.github.com>
2017-11-25 22:46:19 +0200

    Add files via upload

--- Untitled55.m
+++ Untitled55.m
@@ -1,25 +1,33 @@
-%Завдання 1
+%Завдання 1.1
 hndlaxis
+
+%Завдання 1.3
+hndlgraf
+
 %Завдання 2
-%hndgraf
-%Завдання 3
 help xlable
 help dtext
 help title
-%Завдання 4
-x=[0.07:0.1:0.8]
-y=sqrt(1+(4*x).*(sin(x)))
+
+%Завдання 3 
+x=[0.07:0.1:0.8] %3.1
+y=sqrt(1+(4*x).*(sin(x))) %3.1
+subplot(1,1,1)%3.2
+plot(x,y,'ro')
 title('Графік функцій')
 xlabel('x')
 ylabel('y')
+
 x1=[-1.0:0.1:0.0]
 y1=(cos(pi*x1.^2))/sqrt(1-3*x1)
 subplot(2,2,1)
 plot(x,y,'g+')
 subplot(2,2,2)
 plot(x1,y1,'b+')
-%Завдання 5
+%Завдання 4
 help comet
- %t = -pi:pi/200:pi;
-        %comet(t,tan(sin(t))-sin(tan(t)))
+ t = -pi:pi/200:pi;
+        comet(t,tan(sin(t))-sin(tan(t)))
+
+
 
